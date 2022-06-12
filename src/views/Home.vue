ecc253015c4280b58a721ea20b5d675345091292
CC <1935158902@qq.com>
2022-06-12 23:16:38 +0800

    初始化 Doc 的 html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
             <h1>咕噜UI</h1>
             <h2>一个厉害的 UI 框架</h2>
             <p class="actions">
-                <a href="">GitHub</a>
-                <a href="">开始</a>
+                <a href="https://github.com/isCCcc">GitHub</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
