357a9abc8a19c74b7799a79af024c7b5de2dbf83
CC <1935158902@qq.com>
2022-06-20 16:48:40 +0800

    完成 switch 按钮切换

---
+++ src/lib/Switch.vue
@@ -0,0 +1,57 @@
+<template>
+    <button :class="{ checked }" @click="toggle">
+        <span></span>
+    </button>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+    setup() {
+
+        const checked = ref(false);
+        const toggle = () => {
+            checked.value = !checked.value;
+        }
+
+        return { checked, toggle }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+
+button {
+    height: $h;
+    width: $h*2;
+    border: none;
+    background-color: grey;
+    border-radius: $h/2;
+    position: relative;
+}
+
+span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background-color: white;
+    border-radius: $h2/2;
+    transition: left 250ms;
+}
+
+button.checked {
+    background-color: blue;
+}
+
+button.checked>span {
+    left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+    outline: none;
+}
+</style>
