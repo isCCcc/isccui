160c1ecdf1e84cca539e6371ce9321c7d950fb38
CC <1935158902@qq.com>
2022-06-28 11:10:21 +0800

    创建一个可用的Button示例

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -99,27 +99,4 @@ aside {
 main {
     overflow: auto;
 }
-
-// aside {
-//     background: lightblue;
-//     width: 150px;
-//     padding: 16px;
-
-//     >h2 {
-//         margin-bottom: 4px;
-//     }
-
-//     >ol {
-//         >li {
-//             padding: 4px 0;
-//         }
-//     }
-
-//     @media (max-width:500px) {
-//         position: fixed;
-//         top: 0;
-//         left: 0;
-//         padding-top: 70px;
-//     }
-// }
 </style>
