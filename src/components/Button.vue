d785af5e3aae65b78488b5327dc19485d7057413
CC <1935158902@qq.com>
2022-06-28 16:05:45 +0800

    为 button 添加了theme属性，用于区分不同类型的 button，为 button 添加了一个基础样式

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,11 +1,22 @@
 <template>
     <div> Button 示例</div>
-    <div><Button>hello</Button></div>
+    <div>
+        <Button>hello</Button>
+        <Button theme="button">hello</Button>
+        <Button theme="link">hello</Button>
+        <Button theme="text">hello</Button>
+    </div>
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-    components: { Button }
+    components: { Button },
+    setup() {
+        const onClick = () => {
+            alert("hello")
+        }
+        return { onClick }
+    }
 }
 </script>
