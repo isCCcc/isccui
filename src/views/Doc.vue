69edc1fd37bb2d159bc320a582eb9888768b28c5
CC <1935158902@qq.com>
2022-07-12 12:44:32 +0800

    修改 Doc html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -18,16 +18,16 @@
                 <h2>组件列表</h2>
                 <ol>
                     <li>
-                        <router-link to="/doc/switch">Switch 组件</router-link>
+                        <router-link to="/doc/switch">Switch 开关</router-link>
                     </li>
                     <li>
-                        <router-link to="/doc/button">Button 组件</router-link>
+                        <router-link to="/doc/button">Button 按钮</router-link>
                     </li>
                     <li>
-                        <router-link to="/doc/dialog">Dialog 组件</router-link>
+                        <router-link to="/doc/dialog">Dialog 对话框</router-link>
                     </li>
                     <li>
-                        <router-link to="/doc/tabs">Tabs 组件</router-link>
+                        <router-link to="/doc/tabs">Tab 标签</router-link>
                     </li>
                 </ol>
             </aside>
