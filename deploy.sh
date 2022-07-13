3c7d3bc9c6b5dd9756863748fe6514977f0b176b
CC <1935158902@qq.com>
2022-07-13 16:36:19 +0800

    添加deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M master &&
+git remote add origin git@github.com:isCCcc/blanche-ui-website.git &&
+git push -f -u origin master &&
+cd -
+echo https://iscccc.github.io/blanche-ui-website/
