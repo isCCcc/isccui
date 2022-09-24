9478c32a63504c07a5840aa594f6df53f4e2ef43
isCCcc <1935158902@qq.com>
2022-09-24 18:04:55 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m "初始化DatePicker组件样式" &&
-git branch -M master &&
+git branch -M main &&
  git remote add origin git@github.com:isCCcc/blanche-ui-website.git &&
 #git remote add origin git@gitee.com:meatball-c/blanche-ui-website.git &&
-git push -f -u origin master &&
+git push -f -u origin main &&
 cd -
 echo https://iscccc.github.io/blanche-ui-website/
